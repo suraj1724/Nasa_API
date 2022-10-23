538f082c8996d6bac3da50b657ac60333f1c1f95
suraj1724 <113453491+suraj1724@users.noreply.github.com>
2022-10-23 14:35:11 +0530

    repository created in git hub
    grab all files on git hub

---
+++ nasa.js
@@ -0,0 +1,20 @@
+console.log("suraj");
+
+function accessNasa(){
+    fetch("https://api.nasa.gov/mars-photos/api/v1/rovers/curiosity/photos?sol=1000&api_key=DEMO_KEY").then(function(recover){
+        return recover.json();
+    }).then(function(information){
+        for(var info of information.photos){
+            let id1=document.getElementById("h5");
+            let id2=document.getElementById("h6");
+            let photo=document.getElementById("pic");
+            
+          photo.src=info.img_src;
+            id1.innerHTML=info.id;
+            id2.innerHTML=info.camera.name;
+            
+           
+        }
+    })
+    console.log("alisha")
+}
